7dcf4cece2a086a514b64636a9981c8905117d9c
xiong <wakexbear@gmail.com>
2017-08-11 19:20:07 +0800

    Vue 动画效果的使用 动态替换加载组件

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,47 @@
     <p v-text="hello"></p>
     <p v-html="hello"></p>
     {{ status ? 'success' : 'fail' }}
-    <componentA></componentA>
+    <componentA>
+      <p>123</p>
+    </componentA>
     <ul>
       <li v-for="item in list"> {{ item.name + '-' + item.price }} </li>
     </ul>
-    <a v-if="isPartA">partA</a>
-    <a v-else>no data</a>
+        <a v-if="isPartA">partA</a>
+        <a v-else>no data</a>
     <button @click="toggle">toggle</button>
     <button @click="addItem">addItem</button>
-    <input v-model.number="myValue">
-      {{ myValue }}
+    <input v-model="myValue">
+      {{ myValueWithoutNum }}
       <select v-model="selection">
           <option v-for="item in selectOption" :value="item.value">{{ item.text }}</option>
       </select>
       {{ selection }}
+
+      <button @click="toggleCom">fade toggle</button>
+      <transition name="fade" mode="out-in">
+        <div :is="currentView"></div>
+      </transition>
   </div>
 </template>
 
 <script>
 //  import Vue from 'vue'
-import componentA from './components/Hello.vue'
+import comA from './components/Hello.vue'
+import comB from './components/Hi.vue'
 export default {
   components: {
-    componentA: componentA
+    comA, comB
+  },
+  computed: {
+    myValueWithoutNum () {
+      return this.myValue.replace(/\d/g, '')
+    }
   },
   data () {
     return {
+      show: true,
+      currentView: 'com-a',
       selectOption: [
         {
           text: 'apple',
@@ -77,6 +92,13 @@ export default {
     },
     toggle () {
       this.isPartA = !this.isPartA
+    },
+    toggleCom () {
+      if (this.currentView === 'com-a') {
+        this.currentView = 'com-b'
+      } else {
+        this.currentView = 'com-a'
+      }
     }
   }
 }
@@ -85,5 +107,13 @@ export default {
 <style>
 html {
     height: 100%;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease-out;
+}
+
+.fade-enter, .fade-leave-active {
+    opacity: 0;
 }
 </style>
