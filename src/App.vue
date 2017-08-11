0ce0d821dd2a2d53450dd61c9cfaafdbf0e1b6a3
xiong <wakexbear@gmail.com>
2017-08-11 15:34:59 +0800

    V-on,@,methods事件绑定

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,23 @@
     {{ status ? 'success' : 'fail' }}
     <componentA></componentA>
     <ul>
-      <li v-for="(value, key) in objList"> {{ key + '-' + value }} </li>
+      <li v-for="item in list"> {{ item.name + '-' + item.price }} </li>
     </ul>
+    <a v-if="isPartA">partA</a>
+    <a v-else>no data</a>
+    <button @click="toggle">toggle</button>
+    <button @click="addItem">addItem</button>
+    <input v-model.number="myValue">
+      {{ myValue }}
+      <select v-model="selection">
+          <option v-for="item in selectOption" :value="item.value">{{ item.text }}</option>
+      </select>
+      {{ selection }}
   </div>
 </template>
 
 <script>
+//  import Vue from 'vue'
 import componentA from './components/Hello.vue'
 export default {
   components: {
@@ -18,10 +29,23 @@ export default {
   },
   data () {
     return {
+      selectOption: [
+        {
+          text: 'apple',
+          value: 0
+        },
+        {
+          text: 'banana',
+          value: 1
+        }
+      ],
+      selection: null,
+      myValue: '',
       hello: '<span>world</span>',
       hi: 'hi',
       num: 1,
       status: false,
+      isPartA: true,
       list: [
         {
           name: 'apple',
@@ -42,6 +66,17 @@ export default {
         color: 'red',
         weight: 14
       }
+    }
+  },
+  methods: {
+    addItem () {
+      this.list.push({
+        name: 'qweqwe',
+        price: 123
+      })
+    },
+    toggle () {
+      this.isPartA = !this.isPartA
     }
   }
 }
